61e948ba5819523b3676a14f95a60495f2c2402b
haowen.zheng <haowen.zheng@wz-inc.com>
2020-01-29 21:24:19 +0800

    bashrc updates

--- install.sh
+++ install.sh
@@ -10,6 +10,12 @@ echo "[INFO] Make sure vim is 8.0+ with --enable-pythoninterp=yes compiled"
 
 cd $(dirname $(readlink -m $0))
 
+if [ -e ~/.bashrc ]
+then
+    cat ~/.basrhc >> ~/.bashrc.local
+    rm ~/.bashrc
+fi
+
 for i in vim vimrc bashrc gitconfig zshrc screenrc tmux.conf
 do
     if [[ ${i} == vim ]]
